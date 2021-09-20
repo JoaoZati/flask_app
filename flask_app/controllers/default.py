3859e158e4a78d5b054ee9e240cc9b2a23bb6f6d
JoaoZati <joaozati@gmail.com>
2021-09-20 08:22:09 -0300

    Rotas: -Primeira forma de definir name = None para retornar uma url vazia se o usuario não definir nome

--- flask_app/controllers/default.py
+++ flask_app/controllers/default.py
@@ -7,6 +7,10 @@ def index():
     return '<center><p>Hello World</p></center>'
 
 
-@app.route('/teste/<nome>')
-def test(nome):
-    return f'<center><p>Olá {nome}!</p></center>'
+@app.route('/test')
+@app.route('/test/<name>')
+def test(name=None):
+    if name:
+        return f'<center><p>Olá {name}!</p></center>'
+
+    return '<center><p>Olá usuario!</p></center>'
