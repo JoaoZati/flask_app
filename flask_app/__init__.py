9b28f68c0672b3d47806c09d0a60869f56e58538
JoaoZati <joaozati@gmail.com>
2021-09-19 21:27:49 -0300

    Models: - Criado processo de Migrações. - não usei a biblioteca manage - comandos usados: $ flask db init (makemigrations) $ flask db migrate $ flask db update

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
-db = SQLAlchemy(app)  # converter para SQL
 
-from controllers import default
+db = SQLAlchemy(app)  # converter para SQL
+migrate = Migrate(app, db)
+
+from flask_app.controllers import default
+from flask.cli import cli
+
