832bd77f3edbf22b78252d926a4c34c691715d7b
JoaoZati <joaozati@gmail.com>
2021-09-20 09:13:47 -0300

    Tests: -Adicionando teste simples

--- flask_app/controllers/default.py
+++ flask_app/controllers/default.py
@@ -1,30 +1,7 @@
 from flask_app import app
-
+from flask import render_template
 
 @app.route('/index')
 @app.route('/')
 def index():
-    return '<center><p>Hello World</p></center>'
-
-
-@app.route('/test', defaults={'name': None})
-@app.route('/test/<name>')
-def test(name):
-    if name:
-        return f'<center><p>Olá {name}!</p></center>'
-
-    return '<center><p>Olá usuario!</p></center>'
-
-
-@app.route('/test/int/<int:id>', methods=['GET'])
-def test_int(id):
-    print(type(id))
-    return f'''
-    <html>
-    <head>
-        <title>int</title>
-    </head>
-    <body>
-        <center><h1>{id}</h1></center>
-    </body>
-    </html>'''
+    return render_template('templates/index.html')
