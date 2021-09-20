2ecadb51cf5545a036d0843be21177aa4246e6e9
JoaoZati <joaozati@gmail.com>
2021-09-20 08:10:10 -0300

    Configurações de Ambiente: - Criado config.py para configurar o ambiente. - Debug foi ativado e o SQLALCHEMy também.

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,11 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+app.config.from_object('config')
 
 db = SQLAlchemy(app)  # converter para SQL
 migrate = Migrate(app, db)
 
 from flask_app.controllers import default
-from flask.cli import cli
-
