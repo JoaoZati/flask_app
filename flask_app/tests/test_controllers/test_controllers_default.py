bd006fcebaad7ed74a4800443f447e81a5270298
JoaoZati <joaozati@gmail.com>
2021-09-20 12:17:33 -0300

    Templates: - Implementado teste de usuario em test_controllers_default.py (esse deu trabalho...) - Corrigindo arquivo Flake8

--- flask_app/tests/test_controllers/test_controllers_default.py
+++ flask_app/tests/test_controllers/test_controllers_default.py
@@ -20,3 +20,17 @@ def test_home_index(resp_home):
 
 def test_titulo_home(resp_home):
     assert b"Bem Vindo ao Flask app!" in resp_home.data
+
+
+@pytest.fixture(scope="function")
+def user_mock(request):
+    return request.param
+
+
+@pytest.mark.parametrize(
+    'user_mock',
+    ['Joao', 'Guilherme']
+)
+def test_resp_usuario(client, user_mock):
+    resp_usuario = client.get(f'/index/{user_mock}')
+    assert bytes(f'Bem Vindo ao Flask app {user_mock}!', "utf-8") in resp_usuario.data
