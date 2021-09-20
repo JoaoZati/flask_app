0871b3bc6866cd8a550dffc613fccd915932a914
JoaoZati <joaozati@gmail.com>
2021-09-20 08:17:23 -0300

    Rotas: -Definindo parâmetros nas rotas

--- flask_app/controllers/default.py
+++ flask_app/controllers/default.py
@@ -1,6 +1,12 @@
 from flask_app import app
 
+
 @app.route('/index')
 @app.route('/')
 def index():
     return '<center><p>Hello World</p></center>'
+
+
+@app.route('/teste/<nome>')
+def test(nome):
+    return f'<center><p>Olá {nome}!</p></center>'
