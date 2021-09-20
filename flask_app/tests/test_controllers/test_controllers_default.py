34e5528edc4dd7f06275cac628dc15f5b2f64f21
JoaoZati <joaozati@gmail.com>
2021-09-20 10:12:55 -0300

    Templates: - Criando teste para verificar se o nome existe no titulo

--- flask_app/tests/test_controllers/test_controllers_default.py
+++ flask_app/tests/test_controllers/test_controllers_default.py
@@ -8,6 +8,20 @@ def client():
     return app.test_client()
 
 
-def test_index(client):
-    resp = client.get('/')
-    assert resp.status_code == 200
+@pytest.fixture
+def resp_home(client):
+    resp_home = client.get('/')
+    return resp_home
+
+
+def test_home_index(resp_home):
+    assert resp_home.status_code == 200
+
+
+@pytest.mark.parametrize(
+    'name',
+    ['joao', 'guilherme']
+)
+def test_titulo(resp_home, name):
+    assert bytes(f"Bem Vindo {name}!", 'ascii') in resp_home.data
+
